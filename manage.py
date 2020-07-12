af602fed8387b00ae2b9e38f2ff059fa6fa8fb55
qianqian0223 <1042408684@qq.com>
2020-07-12 14:36:34 +0800

    three commit

--- manage.py
+++ manage.py
@@ -2,14 +2,16 @@
 #from flask.ext.script import Manager, Server
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-import main
-import models
+
+from qianblog import app
+from qianblog import models
 
 # Init manager object via app object
-manager = Manager(main.app)
+manager = Manager(app)
 
 # Init migrate object via app and db object
-migrate = Migrate(main.app, models.db)
+migrate = Migrate(app, models.db)
+
 
 # Create a new commands: server
 # This command will be run the Flask development_env server
@@ -24,12 +26,14 @@ def make_shell_context():
     type: `Dict`
     """
     # 确保有导入 Flask app object，否则启动的 CLI 上下文中仍然没有 app 对象
-    return dict(app=main.app,
+    return dict(app=app,
                 db=models.db,
                 User=models.User,
                 Post=models.Post,
                 Comment=models.Comment,
                 Tag=models.Tag)
 
+
 if __name__ == '__main__':
     manager.run()
+
